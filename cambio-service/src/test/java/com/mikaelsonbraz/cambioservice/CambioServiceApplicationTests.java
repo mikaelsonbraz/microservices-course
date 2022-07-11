2295fa7a48bcb52d4f53438f94acfa054d3048f7
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-07-11 13:21:55 -0300

    Adding cambio-service microservice

---
+++ cambio-service/src/test/java/com/mikaelsonbraz/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelsonbraz.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
