48b6d711a96908468d1846e56457425ce11d1c81
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-07-12 10:19:23 -0300

    addig environment port attribute dynamically

--- cambio-service/src/main/java/com/mikaelsonbraz/cambioservice/controllers/CambioController.java
+++ cambio-service/src/main/java/com/mikaelsonbraz/cambioservice/controllers/CambioController.java
@@ -2,6 +2,8 @@ package com.mikaelsonbraz.cambioservice.controllers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mikaelsonbraz.cambioservice.models.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,15 @@ import java.math.BigDecimal;
 @RequestMapping("/cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
+
     // http://localhost:8000/cambio-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
                             @PathVariable("to") String to){
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT: 8000");
+        String port = environment.getProperty("local.server.port");
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
     }
 }
