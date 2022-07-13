6ec15d65459aa8db099004ed34a1d8e01b688417
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-07-13 10:06:24 -0300

    Consuming cambio-microservice by book-microservice through RestTemplate, not recommended

--- book-service/src/main/java/com/mikaelsonbraz/bookservice/controllers/BookController.java
+++ book-service/src/main/java/com/mikaelsonbraz/bookservice/controllers/BookController.java
@@ -2,15 +2,18 @@ package com.mikaelsonbraz.bookservice.controllers;
 
 import com.mikaelsonbraz.bookservice.models.Book;
 import com.mikaelsonbraz.bookservice.repositories.BookRepository;
+import com.mikaelsonbraz.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
@@ -28,8 +31,19 @@ public class BookController {
                         @PathVariable("currency") String currency){
         Optional<Book> book = bookRepository.findById(id);
         if (book.isEmpty()) throw new RuntimeException("Book not found");
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.get().getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+        Cambio cambio = new RestTemplate().getForEntity(
+                "http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio.class,
+                params).getBody();
+
         String port = environment.getProperty("local.server.port");
         book.get().setEnvironment(port);
+        book.get().setPrice(cambio.getConvertedValue());
         return book.get();
     }
 }
---
+++ book-service/src/main/java/com/mikaelsonbraz/bookservice/response/Cambio.java
@@ -0,0 +1,102 @@
+package com.mikaelsonbraz.bookservice.response;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cambio implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long cambioId;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+
+    public Cambio() {
+    }
+
+    public Cambio(Long cambioId, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
+        this.cambioId = cambioId;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getCambioId() {
+        return cambioId;
+    }
+
+    public void setCambioId(Long cambioId) {
+        this.cambioId = cambioId;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactory) {
+        this.conversionFactor = conversionFactory;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cambio cambio = (Cambio) o;
+        return Objects.equals(cambioId, cambio.cambioId) && Objects.equals(from, cambio.from) && Objects.equals(to, cambio.to) && Objects.equals(conversionFactor, cambio.conversionFactor) && Objects.equals(convertedValue, cambio.convertedValue) && Objects.equals(environment, cambio.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cambioId, from, to, conversionFactor, convertedValue, environment);
+    }
+
+    @Override
+    public String toString() {
+        return "Cambio{" +
+                "cambioId=" + cambioId +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionFactor=" + conversionFactor +
+                ", convertedValue=" + convertedValue +
+                ", environment='" + environment + '\'' +
+                '}';
+    }
+}
