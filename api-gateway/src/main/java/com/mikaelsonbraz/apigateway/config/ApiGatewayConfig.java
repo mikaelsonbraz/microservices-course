457691a7e36fec3715f12225c89f0f85718f6af2
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-07-17 11:01:57 -0300

    adding gateway config at api-gateway microservice with gateway router

---
+++ api-gateway/src/main/java/com/mikaelsonbraz/apigateway/config/ApiGatewayConfig.java
@@ -0,0 +1,29 @@
+package com.mikaelsonbraz.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+
+        Function<PredicateSpec, Buildable<Route>> function =  p -> p.path("/get")
+                .filters(f -> f.addRequestHeader("Hello", "World")
+                                .addRequestParameter("Hello", "Nordeste"))
+                .uri("http://httpbin.org:80");
+
+        return routeLocatorBuilder
+                .routes()
+                .route(function)
+                .build();
+    }
+}
