9fcc88a60dae35dd3bcd345f1c78fa7d30efc4e9
mikaelsonbraz <mikaelsonmrn@gmail.com>
2022-07-18 12:21:18 -0300

    Resilience4j retry implemented, book-service's application.yml changed to add a retry max attempts to 8

--- book-service/src/main/java/com/mikaelsonbraz/bookservice/controllers/TesteResilienceController.java
+++ book-service/src/main/java/com/mikaelsonbraz/bookservice/controllers/TesteResilienceController.java
@@ -1,15 +1,27 @@
 package com.mikaelsonbraz.bookservice.controllers;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
 public class TesteResilienceController {
 
+    private Logger logger = LoggerFactory.getLogger(TesteResilienceController.class);
+
     @GetMapping("/teste")
+    @Retry(name = "book-service")
     public String testeReesilience(){
-        return "Teste do Resilience4j";
+        logger.info("Request for test is received!");
+        ResponseEntity<String> response = new RestTemplate()
+                .getForEntity("http://localhost:8080/teste", String.class);
+
+        return response.getBody();
     }
 }
